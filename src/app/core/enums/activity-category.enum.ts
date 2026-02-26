a099200d4663e2a90b9ddeb12111166bffb7afce
JEspinosa1770 <jespinosa1770@gmail.com>
2026-02-26 20:47:49 +0100

    chore: add initial project enums

---
+++ src/app/core/enums/activity-category.enum.ts
@@ -0,0 +1,11 @@
+export enum ActivityCategory {
+  Transporte = 'transporte',
+  Alojamiento = 'alojamiento',
+  Alimentacion = 'alimentación',
+  Entretenimiento = 'entretenimiento',
+  VidaNocturna = 'vida-nocturna',
+  Cultura = 'cultura',
+  Naturaleza = 'naturaleza',
+  Bienestar = 'bienestar',
+  Compras = 'compras',
+}
