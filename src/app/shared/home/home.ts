cbb07d0a31f19621ea75809d6c3e106e3fdda42a
JEspinosa1770 <jespinosa1770@gmail.com>
2026-03-16 12:54:07 +0100

    style(home, navbar): update styles

--- src/app/shared/home/home.ts
+++ src/app/shared/home/home.ts
@@ -11,11 +11,11 @@ import { Slide } from '../../core/models/carrusel.model';
 export class HomeComponent implements OnInit {
   slides = signal<Slide[]>([
     { url: 'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80', alt: 'Montañas majestuosas' },
-    { url: 'https://picsum.photos/id/10/1920/1080', alt: 'Ciudad moderna' },
+    { url: 'https://res.cloudinary.com/dux4gqdow/image/upload/v1773654004/cosas-que-ver-en-segovia-espana_d16fi3.jpg', alt: 'Alcázar de Segovia' },
     // { url: 'https://images.unsplash.com/photo-1449156001533-cb39c8504913?auto=format&fit=crop&q=80', alt: 'Ciudad moderna' },
     { url: 'https://images.unsplash.com/photo-1507525428034-b723cf961d3e?auto=format&fit=crop&q=80', alt: 'Playa tropical' },
-    { url: 'https://picsum.photos/id/28/1920/1080', alt: 'Aventura en el bosque' }
-    // { url: 'https://images.unsplash.com/photo-1500835595327-8113711b062b?auto=format&fit=crop&q=80', alt: 'Aventura en el bosque' }
+    { url: 'https://res.cloudinary.com/dux4gqdow/image/upload/v1773654729/db023d436664aa88b2e84dc0cccdb870491b07e6-1300x865_f0e4fw.webp', alt: 'Meseta de Gizha' },
+    { url: 'https://res.cloudinary.com/dux4gqdow/image/upload/v1773655371/lison-zhao-tGRCnbIsSn4-unsplash_eee4ku.jpg', alt: 'Monumento Sverd i fjell ("Espadas en la roca"), Stavenger, Noruega' },
   ]);
 
   currentIndex = signal<number>(0);
