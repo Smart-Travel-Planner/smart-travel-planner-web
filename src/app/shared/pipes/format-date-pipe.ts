f31a4a4234d1e931a4d80f79dd9c1a08a0ed9167
JEspinosa1770 <jespinosa1770@gmail.com>
2026-03-17 17:58:15 +0100

    refactor(datetime-pipe): update date-time pipe

--- src/app/shared/pipes/format-date-pipe.ts
+++ src/app/shared/pipes/format-date-pipe.ts
@@ -5,8 +5,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatDatePipe implements PipeTransform {
   transform(value: string | undefined | null): string {
+    // if (!value) return '';
+    // const [year, month, day] = value.split('T')[0].split('-');
+    // return `${day}/${month}/${year}`;
     if (!value) return '';
-    const [year, month, day] = value.split('T')[0].split('-');
-    return `${day}/${month}/${year}`;
+    
+    const [datePart, timePart] = value.split('T');
+    const [year, month, day] = datePart.split('-');
+    const formattedDate = `${day}/${month}/${year}`;
+
+    if (timePart) {
+      const [hours, minutes] = timePart.split(':');
+      return `${formattedDate} ${hours}:${minutes}`;
+    }
+
+    return formattedDate;
   }
 }
